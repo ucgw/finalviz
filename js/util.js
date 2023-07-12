89066a0b51a88c3628bd0507817f2903eaf194be
ucgw <ucgw ucgwest@gmail.com>
2023-07-11 20:45:21 -0500

    basic function for getting data promises

--- js/util.js
+++ js/util.js
@@ -1,9 +1,12 @@
 async function read_chart_data() {
-  const data = await d3.csv("https://ucgw.github.io/data/mag6plus_charts.csv");
-  console.log(data);
+  return await d3.csv("https://ucgw.github.io/data/mag6plus_charts.csv");
 }
 
 async function read_record_data() {
-  const data = await d3.csv("https://ucgw.github.io/data/mag6plus_records.csv");
-  console.log(data);
+  return await d3.csv("https://ucgw.github.io/data/mag6plus_records.csv");
+}
+
+function log_promise_data(promise_data) {
+  promise_data
+    .then(function(data) { console.log(data); });
 }
