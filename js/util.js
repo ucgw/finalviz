eb7f694cd4cb4568173b4bb0426368442890df5f
ucgw <ucgw ucgwest@gmail.com>
2023-07-14 21:51:36 -0500

    wip 1

--- js/util.js
+++ js/util.js
@@ -13,16 +13,18 @@ function log_promise_data(promise_data) {
     .then(function(data) { console.log(data); });
 }
 
-function d3_svg_select_data_enter(data, width, height, tx, ty, shape) {
+function d3_svg_select_data_enter(data, width, height, xform_xrange, xform_yrange, shape) {
   var svg = d3.select("svg")
               .attr("width", width)
               .attr("height", height);
 
-  return svg.append("g")
-            .attr("transform", "translate("+tx+","+ty+")")
-            .selectAll(shape)
-            .data(data)
-            .enter();
+  var chart = svg.append("g")
+              .attr("transform", "translate("+xform_xrange+","+xform_yrange+")")
+              .selectAll(shape)
+              .data(data)
+              .enter();
+
+  return { svg, chart };
 }
 
 function d3_append_circles(chart, cx, cxdom, cxrange, cy, cydom,cyrange, r, rrange) {
@@ -41,6 +43,27 @@ function d3_append_circles(chart, cx, cxdom, cxrange, cy, cydom,cyrange, r, rran
        .attr("cx", function(d,i) { return cxln(cx[i]); })
        .attr("cy", function(d,i) { return cy[i]; })
        .attr("r", function(d,i) { return r[i]; })
+}
+
+function d3_append_axis(svg, axistype, xform_xrange, xform_yrange, xd, xr, xt, tformat) {
+  let ax = d3.scaleLog()
+             .base(10)
+             .domain(xd)
+             .range(xr);
+
+  let axis = axistype().scale(ax);
+
+  axis.tickValues(xt);
+  axis.tickFormat(d3.format(tformat));
+
+  let axis_g = svg.append("g")
+         .attr("transform", "translate("+xform_xrange+","+xform_yrange+")")
+         .attr("fill", "none")
+         .attr("font-size", "12")
+         .attr("font-family", "sans-serif")
+         .attr("text-anchor", "middle");
+
+  axis_g.call(axis);
 }
 
 function percentage_diff_calc(x, y, scale) {
@@ -108,6 +131,20 @@ function scatterplot_nstrms_calc(x, y, scale) {
   return sum_dp_square_calc(x, y, scale);
 }
 
+function axis_ticks_calc(xd, numticks) {
+  let unique = xd.filter((x, i) => {
+                 return xd.indexOf(x) === i;
+               });
+  let tickfreq = Math.ceil(unique.length / Number(numticks));
+
+  let ticks = unique.sort().filter((x, i) => {
+           return i % tickfreq === 0;
+         });
+
+  console.log(ticks);
+  return ticks;
+}
+
 function make_scatterplot(promise_data, yaxfield, cxfield, cyfield, xform_xrange, xform_yrange, canvas_width, canvas_height, downsize, logscale, yaxmultiplier, errmultiplier) {
   promise_data
     .then(
@@ -126,10 +163,18 @@ function make_scatterplot(promise_data, yaxfield, cxfield, cyfield, xform_xrange
         let yaxupper = Math.floor(Math.max(...cy)) + 1;
 
         var raderr_sized = [];
+        var xa_domain = [];
+        var ya_domain = [];
+        var xa_range = [];
+        var ya_range = [];
+        var xa_ticks = [];
+        var ya_ticks = [];
 
         switch (cyfield) {
           case "magError":
             raderr_sized = scatterplot_magerror_calc(cx, cy, errmultiplier);
+            xa_ticks = axis_ticks_calc(cx, 3);
+            xa_domain = [Math.min(...cx), Math.max(...cx)];
             break;
 
           case "depthError":
@@ -143,6 +188,9 @@ function make_scatterplot(promise_data, yaxfield, cxfield, cyfield, xform_xrange
 
         let scatter = d3_svg_select_data_enter(cx, canvas_width, canvas_height, xform_xrange, xform_yrange, "circle");
 
-        d3_append_circles(scatter, cx, [cxlower, cxupper], [0, canvas_height], yax, [yaxupper, yaxlower],[0, canvas_height], raderr_sized, [0, canvas_height]);
+        d3_append_circles(scatter.chart, cx, [cxlower, cxupper], [0, canvas_height], yax, [yaxupper, yaxlower],[0, canvas_height], raderr_sized, [0, canvas_height]);
+
+        d3_append_axis(scatter.svg, d3.axisBottom, 50, 300, xa_domain, [0, canvas_height], xa_ticks, '~s');
+
     });
 }
